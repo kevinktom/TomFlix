d19077789223d1c89f85d6f93d3f4d8c25350933
kevin <kevinktom@gmail.com>
2019-10-08 17:58:39 -0400

    Updated CSS

--- frontend/components/videos_index/videos.jsx
+++ frontend/components/videos_index/videos.jsx
@@ -24,6 +24,7 @@ class Videos extends React.Component{
   componentDidMount(){
     // const allProps = this.props;
     this.props.renderVideos();
+    // debugger
 
   }
 
@@ -35,7 +36,7 @@ class Videos extends React.Component{
       <NavBarContainer />
       {this.props.videos.length >= 2 ? 
         <div className="mainVideoDiv">
-          <video controls className="mainVideo"> <source src={this.props.videos[1].video_url} type="video/mp4"/> </video> 
+          <video className="mainVideo"> <source src={this.props.videos[1].video_url} type="video/mp4"/> </video> 
             {/* <div className='mainVideoDescription'>{this.props.videos[1].description}</div> */}
           {/* <Link to={`/browse/${video.id}`}>Play</Link>  */}
           <img src={window.inceptionlogo} className="mainvideoLogo"/>
@@ -48,27 +49,27 @@ class Videos extends React.Component{
             <p className="genreName" >Fantasy/Sci-fi</p>
             <div className='blockrow'>
               <div className='videodiv'>
-                <video className="rowvideo"> <source src={this.props.videos[1].video_url} type="video/mp4" /> </video>
+                <video className="rowvideo" poster={this.props.videos[1].photo_url}> <source src={this.props.videos[1].video_url} type="video/mp4" /> </video>
               </div>
 
               <div className='videodiv'>
-                <video className="rowvideo"> <source src={this.props.videos[2].video_url} type="video/mp4" /> </video>
+                <video className="rowvideo" poster={this.props.videos[2].photo_url}> <source src={this.props.videos[2].video_url} type="video/mp4" /> </video>
               </div>
 
               <div className='videodiv'>
-                <video className="rowvideo"> <source src={this.props.videos[4].video_url} type="video/mp4" /> </video>
+                <video className="rowvideo" poster={this.props.videos[10].photo_url}> <source src={this.props.videos[10].video_url} type="video/mp4" /> </video>
               </div>
 
               <div className='videodiv'>
-                <video className="rowvideo"> <source src={this.props.videos[9].video_url} type="video/mp4" /> </video>
+                <video className="rowvideo" poster={this.props.videos[9].photo_url}> <source src={this.props.videos[9].video_url} type="video/mp4" /> </video>
               </div>
 
               <div className='videodiv'>
-                <video className="rowvideo"> <source src={this.props.videos[10].video_url} type="video/mp4" /> </video>
+                <video className="rowvideo" poster={this.props.videos[4].photo_url}> <source src={this.props.videos[4].video_url} type="video/mp4" /> </video>
               </div>
 
               <div className='videodiv'>
-                <video className="rowvideo"> <source src={this.props.videos[11].video_url} type="video/mp4" /> </video>
+                <video className="rowvideo" poster={this.props.videos[11].photo_url}> <source src={this.props.videos[11].video_url} type="video/mp4" /> </video>
               </div>
             </div>
           </div>
@@ -77,7 +78,7 @@ class Videos extends React.Component{
 
 
         </div> : <div className='mainBackground'> </div>}
-      <h1>Welcome to the best version of Netflix</h1>
+      {/* <h1>Welcome to the best version of Netflix</h1> */}
         {/* {this.props.videos.length >= 1 ?
           <div>
       <video controls className='testvid' > <source src={this.props.videos[0].video_url} type="video/mp4" /> </video> 
