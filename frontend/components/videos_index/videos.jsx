19db14b88305388576c78210f3eb475c6ea07831
kevin <kevinktom@gmail.com>
2019-10-08 17:31:49 -0400

    Created video genre rows

--- frontend/components/videos_index/videos.jsx
+++ frontend/components/videos_index/videos.jsx
@@ -34,7 +34,7 @@ class Videos extends React.Component{
     <div className='indexGrid'>
       <NavBarContainer />
       {this.props.videos.length >= 2 ? 
-        <div>
+        <div className="mainVideoDiv">
           <video controls className="mainVideo"> <source src={this.props.videos[1].video_url} type="video/mp4"/> </video> 
             {/* <div className='mainVideoDescription'>{this.props.videos[1].description}</div> */}
           {/* <Link to={`/browse/${video.id}`}>Play</Link>  */}
@@ -44,17 +44,44 @@ class Videos extends React.Component{
 
 
 
-          <div className="rowsection"> row </div>
+          <div className="rowsection"> 
+            <p className="genreName" >Fantasy/Sci-fi</p>
+            <div className='blockrow'>
+              <div className='videodiv'>
+                <video className="rowvideo"> <source src={this.props.videos[1].video_url} type="video/mp4" /> </video>
+              </div>
+
+              <div className='videodiv'>
+                <video className="rowvideo"> <source src={this.props.videos[2].video_url} type="video/mp4" /> </video>
+              </div>
+
+              <div className='videodiv'>
+                <video className="rowvideo"> <source src={this.props.videos[4].video_url} type="video/mp4" /> </video>
+              </div>
+
+              <div className='videodiv'>
+                <video className="rowvideo"> <source src={this.props.videos[9].video_url} type="video/mp4" /> </video>
+              </div>
+
+              <div className='videodiv'>
+                <video className="rowvideo"> <source src={this.props.videos[10].video_url} type="video/mp4" /> </video>
+              </div>
+
+              <div className='videodiv'>
+                <video className="rowvideo"> <source src={this.props.videos[11].video_url} type="video/mp4" /> </video>
+              </div>
+            </div>
+          </div>
           
 
 
 
         </div> : <div className='mainBackground'> </div>}
       <h1>Welcome to the best version of Netflix</h1>
-        {this.props.videos.length >= 1 ?
+        {/* {this.props.videos.length >= 1 ?
           <div>
-      <video controls > <source src={this.props.videos[0].video_url} type="video/mp4" /> </video> 
-          </div> : <div className='mainBackground'> </div>}
+      <video controls className='testvid' > <source src={this.props.videos[0].video_url} type="video/mp4" /> </video> 
+          </div> : <div className='mainBackground'> </div>} */}
       {/* <button onClick={this.props.logoutCurrentUser}>Logout</button> */}
     </div>
   )
