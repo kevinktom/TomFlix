6e5417fb5b5500057acfa37a48cfd2059c0e742a
kevin <kevinktom@gmail.com>
2019-10-08 18:52:49 -0400

    Added the ability to play the videos on hover

--- frontend/components/videos_index/videos.jsx
+++ frontend/components/videos_index/videos.jsx
@@ -17,9 +17,17 @@ import NavBarContainer from '../navbar/navbar_container'
 class Videos extends React.Component{
   constructor(props){
     super(props);
+    this.handleHoverPlay = this.handleHoverPlay.bind(this);
+    this.handleHoverLeave = this.handleHoverLeave.bind(this);
   }
 
+  handleHoverPlay(e){
+    e.currentTarget.play();
+  }
 
+  handleHoverLeave(e){
+    e.currentTarget.pause();
+  }
   
   componentDidMount(){
     // const allProps = this.props;
@@ -49,6 +57,38 @@ class Videos extends React.Component{
             <p className="genreName" >Fantasy/Sci-fi</p>
             <div className='blockrow'>
               <div className='videodiv'>
+                <video className="rowvideo" poster={this.props.videos[1].photo_url} onMouseOver={this.handleHoverPlay} onMouseLeave={this.handleHoverLeave}> <source src={this.props.videos[1].video_url} type="video/mp4" /> </video>
+              </div>
+
+              <div className='videodiv'>
+                <video className="rowvideo" poster={this.props.videos[2].photo_url} onMouseOver={this.handleHoverPlay} onMouseLeave={this.handleHoverLeave}> <source src={this.props.videos[2].video_url} type="video/mp4" /> </video>
+              </div>
+
+              <div className='videodiv'>
+                <video className="rowvideo" poster={this.props.videos[10].photo_url} onMouseOver={this.handleHoverPlay} onMouseLeave={this.handleHoverLeave}> <source src={this.props.videos[10].video_url} type="video/mp4" /> </video>
+              </div>
+
+              <div className='videodiv'>
+                <video className="rowvideo" poster={this.props.videos[9].photo_url} onMouseOver={this.handleHoverPlay} onMouseLeave={this.handleHoverLeave}> <source src={this.props.videos[9].video_url} type="video/mp4" /> </video>
+              </div>
+
+              <div className='videodiv'>
+                <video className="rowvideo" poster={this.props.videos[4].photo_url} onMouseOver={this.handleHoverPlay} onMouseLeave={this.handleHoverLeave}> <source src={this.props.videos[4].video_url} type="video/mp4" /> </video>
+              </div>
+
+              <div className='videodiv'>
+                <video className="rowvideo" poster={this.props.videos[11].photo_url} onMouseOver={this.handleHoverPlay} onMouseLeave={this.handleHoverLeave}> <source src={this.props.videos[11].video_url} type="video/mp4" /> </video>
+              </div>
+            </div>
+          </div>
+
+
+          
+
+          {/* <div className="secondsection">
+            <p className="genreName" >Fantasy/Sci-fi</p>
+            <div className='blockrow'>
+              <div className='videodiv'>
                 <video className="rowvideo" poster={this.props.videos[1].photo_url}> <source src={this.props.videos[1].video_url} type="video/mp4" /> </video>
               </div>
 
@@ -72,12 +112,13 @@ class Videos extends React.Component{
                 <video className="rowvideo" poster={this.props.videos[11].photo_url}> <source src={this.props.videos[11].video_url} type="video/mp4" /> </video>
               </div>
             </div>
-          </div>
+          </div> */}
           
 
 
 
         </div> : <div className='mainBackground'> </div>}
+        <div className="bottomhalf"></div>
       {/* <h1>Welcome to the best version of Netflix</h1> */}
         {/* {this.props.videos.length >= 1 ?
           <div>
