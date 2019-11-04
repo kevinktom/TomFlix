d70156d8a284bf3a693c5d355e0b4c5e6f4d5eb4
kevin <kevinktom@gmail.com>
2019-11-04 16:22:27 -0500

    Refactored for DRY mylist state

--- frontend/components/videos_index/videos.jsx
+++ frontend/components/videos_index/videos.jsx
@@ -24,6 +24,7 @@ class Videos extends React.Component{
     this.handleMute = this.handleMute.bind(this);
     this.handleMyList = this.handleMyList.bind(this);
     this.handleVideoList = this.handleVideoList.bind(this);
+    this.checkMyList = this.checkMyList.bind(this);
   }
 
   handleHoverPlay(e){
@@ -80,16 +81,25 @@ class Videos extends React.Component{
   }
 
   handleMyList(){
-    // let listbutton = document.getElementById("addlistbutton")
-    // debugger
-    if (this.state.indexlist) {
+    let currState = this.state.listchange.slice();
+    if (currState[0]) {
       this.props.deleteMyList(this.props.videos[0].id).then(this.props.fetchLists);
-      this.setState({ indexlist: false }); // batch set state after the loop ****
+      currState[0] = false;
+      this.setState({ listchange: currState }); // batch set state after the loop ****
     }
     else {
       this.props.createMyList(this.props.videos[0].id).then(this.props.fetchLists);
-      this.setState({ indexlist: true });
+      currState[0] = true;
+      this.setState({ listchange: currState });
     }
+    // if (this.state.indexlist) {
+    //   this.props.deleteMyList(this.props.videos[0].id).then(this.props.fetchLists);
+    //   this.setState({ indexlist: false }); // batch set state after the loop ****
+    // }
+    // else {
+    //   this.props.createMyList(this.props.videos[0].id).then(this.props.fetchLists);
+    //   this.setState({ indexlist: true });
+    // }
   }
 
   handleVideoList(video){
@@ -125,51 +135,71 @@ class Videos extends React.Component{
     }
     // debugger
   }
+
+  checkMyList(){
+    let currentState = this.state.listchange.slice();
+    this.props.videos.forEach((video, idx) => {
+      currentState[idx] = false;
+      this.props.mylists.forEach(list => {
+        if (list.video_id === video.id) {
+          // indexList = true;
+          currentState[idx] = true; // change to setState
+          //   return;
+
+          // }
+          // else {
+        }
+      })
+
+    })
+    // debugger
+    this.setState({ listchange: currentState });
+  }
   
   componentDidMount(){
     // const allProps = this.props;
     this.props.renderVideos();
     // debugger
     this.props.fetchLists();
-    // debugger
-
-
+    debugger
     if (this.props.videos.length > 0 && this.props.mylists.length > 0) {
        
 
-        let indexliststatus = false;
-        // debugger
-        // indexliststatus = false;
-        for (let i = 0; i < this.props.mylists.length; i++) {
-          if (this.props.mylists[i].video_id === this.props.videos[0].id) {
-            indexliststatus = true;
-            //   break;
-            // }
-            // else {
-          }
-        }
-        if (this.state.indexlist !== indexliststatus) {
-          this.setState({ indexlist: indexliststatus })
-        }
+        // let indexliststatus = false;
+        // // debugger
+        // // indexliststatus = false;
+        // for (let i = 0; i < this.props.mylists.length; i++) {
+        //   if (this.props.mylists[i].video_id === this.props.videos[0].id) {
+        //     indexliststatus = true;
+        //     //   break;
+        //     // }
+        //     // else {
+        //   }
+        // }
+        // if (this.state.indexlist !== indexliststatus) {
+        //   this.setState({ indexlist: indexliststatus })
+        // }
 
 
-        let currentState = this.state.listchange.slice();
-        this.props.videos.forEach((video, idx) => {
-          currentState[idx] = false;
-          this.props.mylists.forEach(list => {
-            if (list.video_id === video.id) {
-              // indexList = true;
-              currentState[idx] = true; // change to setState
-              //   return;
+        // let currentState = this.state.listchange.slice();
+        // this.props.videos.forEach((video, idx) => {
+        //   currentState[idx] = false;
+        //   this.props.mylists.forEach(list => {
+        //     if (list.video_id === video.id) {
+        //       // indexList = true;
+        //       currentState[idx] = true; // change to setState
+        //       //   return;
 
-              // }
-              // else {
-            }
-          })
+        //       // }
+        //       // else {
+        //     }
+        //   })
 
-        })
-        // debugger
-        this.setState({ listchange: currentState });
+        // })
+        // // debugger
+        // this.setState({ listchange: currentState });
+
+        this.checkMyList();
 
       }
 
@@ -181,39 +211,41 @@ class Videos extends React.Component{
     if (this.props.videos.length > 0 && this.props.mylists.length > 0){
       if (prevProps.mylists.length !== this.props.mylists.length){
         
-        let indexliststatus = false;
-        // debugger
-        // indexliststatus = false;
-        for (let i = 0; i < this.props.mylists.length; i++) {
-          if (this.props.mylists[i].video_id === this.props.videos[0].id) {
-            indexliststatus = true;
-          //   break;
-          // }
-          // else {
-          }
-        }
-        if (this.state.indexlist !== indexliststatus){
-          this.setState({ indexlist: indexliststatus })
-        }
+        // let indexliststatus = false;
+        // // debugger
+        // // indexliststatus = false;
+        // for (let i = 0; i < this.props.mylists.length; i++) {
+        //   if (this.props.mylists[i].video_id === this.props.videos[0].id) {
+        //     indexliststatus = true;
+        //   //   break;
+        //   // }
+        //   // else {
+        //   }
+        // }
+        // if (this.state.indexlist !== indexliststatus){
+        //   this.setState({ indexlist: indexliststatus })
+        // }
 
 
-        let currentState = this.state.listchange.slice();
-        this.props.videos.forEach((video, idx) => {
-          currentState[idx] = false;
-          this.props.mylists.forEach(list => {
-            if (list.video_id === video.id) {
-              // indexList = true;
-              currentState[idx] = true; // change to setState
-            //   return;
+        // let currentState = this.state.listchange.slice();
+        // this.props.videos.forEach((video, idx) => {
+        //   currentState[idx] = false;
+        //   this.props.mylists.forEach(list => {
+        //     if (list.video_id === video.id) {
+        //       // indexList = true;
+        //       currentState[idx] = true; // change to setState
+        //     //   return;
 
-            // }
-            // else {
-            }
-          })
+        //     // }
+        //     // else {
+        //     }
+        //   })
 
-        })
-        // debugger
-        this.setState({ listchange: currentState });
+        // })
+        // // debugger
+        // this.setState({ listchange: currentState });
+
+        this.checkMyList();
 
       }
 
@@ -299,7 +331,7 @@ class Videos extends React.Component{
             <p >{IndividualVideos[0].props.video.maturity_rating}</p>
           </div>
           <div onClick= { () => this.props.history.push(`/browse/${IndividualVideos[0].props.video.id}`)} className="playButton transparentPlay"> <img src={window.playicon}/> <p className="playText">Play</p> </div>
-          {this.state.indexlist ? 
+          {this.state.listchange[0] ? 
               <div onClick={this.handleMyList} className="playButton transparentPlay" id="addlistbutton"> <img src={window.indexListRemove} /> <p className="playText">My List</p> </div> :
               <div onClick={this.handleMyList} className="playButton transparentPlay" id="addlistbutton"> <img src={window.indexListAdd} /> <p className="playText">My List</p> </div> }
           {/* {this.props.mylists.forEach(list => {
